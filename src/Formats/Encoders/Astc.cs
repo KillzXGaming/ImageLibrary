3acde9b7ebefeeb69f5469256dcf85cc24cb587f
KillzXGaming <millernathan35@gmail.com>
2026-05-25 16:06:44 -0400

    Use AstcEncoderCSharp for ASTC encoder (very fast)

--- src/Formats/Encoders/Astc.cs
+++ src/Formats/Encoders/Astc.cs
@@ -1,8 +1,8 @@
-﻿using BCnEncoder.Shared;
+﻿using AstcEncoder;
+using BCnEncoder.Shared;
 using ImageLibrary.Helpers;
 using ImageLibrary.Interfaces;
 using ImageLibrary.Native;
-using PVRTexLib;
 using Ryujinx.Graphics.Gal.Texture;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Png;
@@ -14,6 +14,8 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using static ImageLibrary.Formats.Encoders.ImageDds;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ImageLibrary.Formats.Encoders
 {
@@ -60,30 +62,69 @@ namespace ImageLibrary.Formats.Encoders
 
         public byte[] Decode(byte[] data, uint width, uint height)
         {
-            PVRTexture texture = PvrTextureHelper.CreateTexture(data, (PVRTexLibPixelFormat)Format, width, height)
-                     ?? throw new InvalidOperationException("Failed to create astc texture with PVRTexLib.");
+            AstcencSwizzle swizzle = new AstcencSwizzle()
+            {
+                r = AstcencSwz.AstcencSwzR,
+                g = AstcencSwz.AstcencSwzG,
+                b = AstcencSwz.AstcencSwzB,
+                a = AstcencSwz.AstcencSwzA
+            };
+            AstcencError status = Astcenc.AstcencConfigInit(AstcencProfile.AstcencPrfLdr, 
+                BlockWidth, BlockHeight, BlockDepth, Astcenc.AstcencPreMedium, 0, out AstcencConfig config);
+            
+            AstcencContext context;
+            status = Astcenc.AstcencContextAlloc(ref config, 1, out context);
 
-            bool successful = texture.Transcode(PvrTextureHelper.Rgba8888, 
-                PVRTexLibVariableType.UnsignedByteNorm, 
-                PVRTexLibColourSpace.Linear,
-                PVRTexLibCompressorQuality.PVRTCHigh);
-            if (!successful)
-                throw new InvalidOperationException("Transcoding with PVRTexLib was not successful.");
+            byte[] output = new byte[width * height * 4];
 
-            return PvrTextureHelper.GetData(texture);
+            AstcencImage outImage;
+            outImage.dimX = width;
+            outImage.dimY = height;
+            outImage.dimZ = 1;
+            outImage.dataType = AstcencType.AstcencTypeU8;
+            outImage.data = output;
+            status = Astcenc.AstcencDecompressImage(context, data, ref outImage, swizzle, 0);
+
+            Astcenc.AstcencContextFree(context);
+
+            return output;
         }
 
         public byte[] Encode(byte[] data, uint width, uint height)
         {
-            PVRTexture texture = PvrTextureHelper.CreateTexture(data, PvrTextureHelper.Rgba8888, width, height)
-                     ?? throw new InvalidOperationException("Failed to create astc texture with PVRTexLib.");
+            uint thread_count = 1;
 
-            texture.Transcode((ulong)Format,
-                PVRTexLibVariableType.UnsignedByteNorm,
-                PVRTexLibColourSpace.Linear,
-                PVRTexLibCompressorQuality.PVRTCHigh);
+            AstcencSwizzle swizzle = new AstcencSwizzle()
+            {
+                r = AstcencSwz.AstcencSwzR,
+                g = AstcencSwz.AstcencSwzG,
+                b = AstcencSwz.AstcencSwzB,
+                a = AstcencSwz.AstcencSwzA
+            };
 
-            return PvrTextureHelper.GetData(texture);
+            AstcencError status = Astcenc.AstcencConfigInit(AstcencProfile.AstcencPrfLdr,
+                BlockWidth, BlockHeight, BlockDepth, Astcenc.AstcencPreMedium, 0, out AstcencConfig config);
+
+            AstcencContext context;
+            status = Astcenc.AstcencContextAlloc(ref config, thread_count, out context);
+
+            uint block_count_x = ((uint)width + BlockWidth - 1) / BlockWidth;
+            uint block_count_y = ((uint)height + BlockHeight - 1) / BlockHeight;
+
+            uint compLen = block_count_x * block_count_y * 16;
+            byte[] comp_data = new byte[compLen];
+
+            AstcencImage outImage;
+            outImage.dimX = width;
+            outImage.dimY = height;
+            outImage.dimZ = 1;
+            outImage.dataType = AstcencType.AstcencTypeU8;
+            outImage.data = data;
+            status = Astcenc.AstcencCompressImage(context, ref outImage, swizzle, comp_data, 0);
+
+            Astcenc.AstcencContextFree(context);
+
+            return comp_data;
         }
 
         public enum AstcFormat
